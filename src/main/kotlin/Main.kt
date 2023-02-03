fab021ca78bf97d7c485ae175854d33157b7ed9a
LilianKanyua <liliankanyua@gmail.com>
2023-02-03 11:11:42 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var fullName= arrayOf("James Muchemi", "Mary Naiserian", "Ben Otieno" )
+    println(fullName)
+
+
+    var ages=arrayOf(23, 25, 30)
+    println(ages)
+
+
+    var phoneNumber: Array<Long> = arrayOf(712345678L, 743567290L, 732147890L)
+    println(phoneNumber)
+
+
+    var weight= arrayOf(80, 75, 80)
+    println(weight)
+
+
+    var citizen: Array<Boolean> = arrayOf(true, false,false )
+    println(citizen)
+
+
+}
